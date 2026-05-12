9f1499bc61713448ae7c272852f113c5c9159fd9
younsoo <younsoo7795@gmail.com>
2026-05-12 10:47:28 +0900

    fix. ViewModel integration missing

--- Views/SettingPage.xaml.cs
+++ Views/SettingPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FaceSearchApp.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,10 @@ namespace FaceSearchApp.Views
     /// </summary>
     public partial class SettingPage : Page
     {
-        public SettingPage()
+        public SettingPage(SettingViewModel vm)
         {
             InitializeComponent();
+            DataContext = vm;
         }
     }
 }
